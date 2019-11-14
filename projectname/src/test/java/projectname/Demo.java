a6bbd47f84dac3bf36a2dd44adc37b1790c11491
pavan kumar <pavan kumar@LAPTOP-QVKC38NK>
2019-11-14 17:37:52 +0530

    goola

---
+++ projectname/src/test/java/projectname/Demo.java
@@ -0,0 +1,14 @@
+package projectname;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+	@Test
+	public void test()
+	{
+		//Reporter.log("hi",true);
+		System.out.println("hi");
+	}
+
+}
